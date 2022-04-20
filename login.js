07a84c1eeda306508ba99e4700d6584b0c19b8cf
vincentevoid <vincentevoid@gmail.com>
2022-04-20 01:43:43 -0500

    Updated wired

--- login.js
+++
@@ -1,9 +0,0 @@
-var urlSelect = document.getElementById('selectUser'),
-myFrame = document.getElementById('myFrame');
-
-function loadPages() {
-    myFrame.src = "";
-    setTimeout(function () {
-        myFrame.src = urlSelect.value;
-    }, 0);
-}
